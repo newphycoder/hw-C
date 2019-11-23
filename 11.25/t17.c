8e129bae1615a0486af756e6cd13e2f9c8e36d1e
newphycoder <wlwa7273@mail.ustc.edu.cn>
2019-11-23 16:45:17 +0800

    19.11.23.02

--- 11.25/t17.c
+++ 11.25/t17.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int sort(int* a,int size);
+int exchange(int* order,int* a,int len);
 
 int main(){
 	int i,j;
@@ -35,52 +36,104 @@ int main(){
 }
 
 int sort(int* a,int n){
-	int i;
-	int Nmax,Nfirst,Nsecond,Nthird,Nfouth;
-	int max,first,second,third,fouth;
-	Nmax=0,Nfirst=0,Nsecond=1,Nthird=2,Nfouth=3;
-	for(i=1;i<n*n;i++)
+	int* order;
+	order = malloc( n*n * int );
+	for (int i = 0; i < n*n-1 ; ++i)
 	{
-		if(a[i]<a[Nfirst])
+		order[i]=i;
+	}
+	for (int i = 0; i < n*n-1; ++i)
+	{
+		for(int j =i; j< n*n-1; ++j)
 		{
-			Nfouth=Nthird;
-			Nthird=Nsecond;
-			Nsecond=Nfirst;
-			Nfirst=i;
-		}
-		else if(a[i]<a[Nsecond])
-		{
-			Nfouth=Nthird;
-			Nthird=Nsecond;
-			Nsecond=i;
-		}
-		else if(a[i]<a[Nthird])
-		{
-			Nfouth=Nthird;
-			Nthird=i;
-		}
-		else if(a[i]<a[Nfouth])
-		{
-			Nfouth=i;
-		}
-		if(a[i]>a[Nmax])
-		{
-			Nmax=i;
+			if(a[order[j]]>a[order[j+1]])
+			{
+				t=order[j];
+				order[j]=order[j+1];
+				order[j+1]=t;
+			}
 		}
 	}
-	first=a[Nfirst];
-	second=a[Nsecond];
-	third=a[Nthird];
-	fouth=a[Nfouth];
-	max=a[Nmax];
-	a[Nfirst]=a[0];
-	a[Nsecond]=a[n-1];
-	a[Nthird]=a[n*(n-1)];
-	a[Nfouth]=a[n*n-1];
-	a[Nmax]=a[n*(n/2)+n/2];
-	a[0]=first;
-	a[n-1]=second;
-	a[n*(n-1)]=third;
-	a[n*n-1]=fouth;
-	a[n*(n/2)+n/2]=max;
+
+	for(int i =0; i< n*n-1; ++i)
+	{
+		if(order[i]=0)
+		{
+			m=i;
+			break;
+		}
+	}
+	_a=a[order[0]];
+	t=order[0];
+	a[order[0]]=a[0];
+	order[0]=0;
+	a[0]=_a;
+	order[m]=t;
+
+	for(int i =0; i< n*n-1; ++i)
+	{
+		if(order[i]=n-1)
+		{
+			m=i;
+			break;
+		}
+	}
+	_a=a[order[1]];
+	t=order[1];
+	a[order[1]]=a[n-1];
+	order[1]=n-1;
+	a[n-1]=_a;
+	order[m]=t;
+
+	for(int i =0; i< n*n-1; ++i)
+	{
+		if(order[i]=n*(n-1))
+		{
+			m=i;
+			break;
+		}
+	}
+	_a=a[order[2]];
+	t=order[2];
+	a[order[2]]=a[n*(n-1)];
+	order[2]=n*(n-1);
+	a[n*(n-1)]=_a;
+	order[m]=t;
+
+	for(int i =0; i< n*n-1; ++i)
+	{
+		if(order[i]=n*n-1)
+		{
+			m=i;
+			break;
+		}
+	}
+	_a=a[order[3]];
+	t=order[3];
+	a[order[3]]=a[n*n-1];
+	order[3]=n*n-1;
+	a[n*n-1]=_a;
+	order[m]=t;
+
+}
+
+int exchange(int* order,int* a,int len,int num){
+	int t;
+	int m;
+	int _a;
+	for(int i =0; i< n*n-1; ++i)
+	{
+		if(order[i]=len)
+		{
+			m=i;
+			break;
+		}
+	}
+	_a=a[order[num]];
+	t=order[num];
+	a[order[num]]=a[len];
+	order[num]=len;
+	a[num]=_a;
+	order[m]=t;
+
 }
