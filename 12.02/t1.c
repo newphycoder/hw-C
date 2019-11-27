5b04b580fc3236152c6a32949ae96610dd8ca24e
newphycoder <wlwa7273@gmail.com>
2019-11-27 22:21:22 +0800

    19.11.27.03

---
+++ 12.02/t1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define count (10)
+
+struct Book
+{
+	int number;
+	char name[30];
+	char author[30];
+	char publisher[30];
+	float price;
+};
+
+int main(){
+	int i;
+	struct Book books[count];
+	for( i=0; i<count; i++)
+	{
+		printf("the %dth book's number\n",i+1);
+		scanf("%d",&books[i].number);
+		printf("the %dth book's name\n",i+1);
+		scanf("%s",books[i].name);
+		printf("the %dth book's author\n",i+1);
+		scanf("%s",books[i].author);
+		printf("the %dth book's publisher\n",i+1);
+		scanf("%s",books[i].publisher);
+		printf("the %dth book's price\n",i+1);
+		scanf("%f",&books[i].price);
+	}
+
+	float sum=0;
+	for( i=0; i<count; i++)
+	{
+		sum+=books[i].price;
+	}
+
+	float average;
+	average = sum/count;
+	printf("the average of the books' price:%f\n",average);
+
+	return (0); 
+}
