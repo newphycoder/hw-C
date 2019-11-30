7176a642f2a447eaeef84120a956cea99a250ef9
newphycoder <wlwa7273@gmail.com>
2019-11-30 15:45:57 +0800

    19.11.30.01

--- 12.02/t4.c
+++ 12.02/t4.c
@@ -13,18 +13,18 @@ struct student
 	float grade;
 };
 
-int input(struct student *sp);
-int sort(struct student *sp);
-int output(struct student *sp);
+int input(struct student *sp[]);
+int sort(struct student *sp[]);
+int output(struct student *sp[]);
 
 int main(){
-	struct student (*sp)[total];
+	struct student *sp[total];
 	struct student stu[total];
 	
 	int i;
 	for( i=0; i<total; i++)
 	{
-		sp = stu;
+		sp[i] = stu+i;
 	}
 
 	input(sp);
@@ -34,41 +34,79 @@ int main(){
 	return 0;
 }
 
-int input(struct student *sp){
+int input(struct student *sp[]){
 	int i;
 	for( i=0; i<total; i++)
 	{
 		printf("input the num of %dth student\n",i+1);
-		scanf("%d",&sp->num);
+		scanf("%d",&sp[i]->num);
 		printf("input the name of %dth student\n",i+1);
-		scanf("%s",sp->name);
+		scanf("%s",sp[i]->name);
+		fflush(stdin);
 		printf("input the sex of %dth student\n",i+1);
-		scanf("%c",&sp->sex);
+		scanf("%c",&sp[i]->sex);
 		printf("input the age of %dth student\n",i+1);
-		scanf("%d",&sp->age);
+		scanf("%d",&sp[i]->age);
 		printf("input the grade of %dth student\n",i+1);
-		scanf("%f",&sp->grade);
-		sp++;
+		scanf("%f",&sp[i]->grade);
 	}
 	return (0);
 }
 
-int sort(struct student *sp){
+int sort(struct student *sp[]){
 	int i,j;
 #ifdef choose
 	int order;
-	struct student media;
+	struct student *media = NULL;
 	for( i=0; i<total; i++)
 	{
 		order = i;
-		media = (sp+i);
+		media = sp[i];
 		for( j=i+1; j<total; j++)
 		{
-			if((sp+j)->grade < media->grade )
+			if( sp[j]->grade < media->grade)
 			{
-				media = (sp+j);
+				media = sp[j];
+				order = j;
 			}
 		}
-		
+		sp[order] = sp[i];
+		sp[i] = media;
 	}
+#else
+	struct student *media = NULL;
+	for( i=0; i<total; i++)
+	{
+		for( j=1; j<total; j++)
+		{
+			if( sp[j-1]->grade < sp[j]->grade)
+			{
+				media = sp[j];
+				sp[j] = sp[j-1];
+				sp[j-1] = media;
+			}
+		}
+	}
+#endif
+	return (0);
+}
 
+int output(struct student *sp[]){
+	int i;
+	printf("num\t");
+	printf("name\t");
+	printf("sex\t");
+	printf("age\t");
+	printf("grade\t");
+	printf("\n");
+	for ( i=0; i<total; i++)
+	{
+		printf("%d\t",sp[i]->num);
+		printf("%s\t",sp[i]->name);
+		printf("%c\t",sp[i]->sex);
+		printf("%d\t",sp[i]->age);
+		printf("%3.1f\t",sp[i]->grade);
+		printf("\n");
+	}
+	return (0);
+}
--- 12.02/t7.c
+++ 12.02/t7.c
@@ -3,7 +3,6 @@
 
 struct Book
 {
-	struct Book *last;
 	int num;
 	char name[30];
 	char author[30];
@@ -11,28 +10,31 @@ struct Book
 	struct Book *next;
 };
 
-int run(struct Book **head);
+int run(struct Book **hp);
 struct Book *add(struct Book *head);
 struct Book *find(struct Book *head,int target);
 int delect(struct Book **head,int target);
-int freememory(head);
+int print(struct Book *head);
+int freememory(struct Book *head);
 
 int main(){
 	struct Book *head = NULL;
 	run(&head);
 	freememory(head);
-
 	return (0);
 }
 
-int run(struct Book **ph){
+int run(struct Book **hp){
 	int i;
+	int num;
+	printf("\n");
 	printf("which operate do you want:\n");
 	printf("0.terminate\n");
 	printf("1.add a book\n");
 	printf("2.find a book\n");
 	printf("3.delect a book\n");
 	printf("4.print all book\n");
+	printf("input:");
 	scanf("%d",&i);
 	if (i==0)
 	{
@@ -40,65 +42,86 @@ int run(struct Book **ph){
 	}
 	else if (i==1)
 	{
-		int n;
-		printf("the number of books\n");
-		scanf("%d",&n);
-		*hp = add(*hp)
+		printf("\n");
+		*hp = add(*hp);
 	}
 	else if (i==2)
 	{
+		printf("\n");
 		struct Book *result;
 		printf("the number of the book\n");
-		scanf("%d",num);
+		scanf("%d",&num);
 		result = find(*hp,num);
 		if (result)
 		{
-			printf("number     :  %s\n",result->number);
+			printf("number     :  %d\n",result->num);
 			printf("name       :  %s\n",result->name);
 			printf("author     :  %s\n",result->author);
 			printf("publisher  :  %s\n",result->publisher);
 		}
 		else
 		{
-			printf("can't find\n");
+			printf("haven't the book\n");
 		}
 	}
 	else if (i==3)
 	{
-		delect(hp,num);
+		int t;
+		printf("\n");
+		printf("the number of the book\n");
+		scanf("%d",&num);
+		t=delect(hp,num);
+		if (t!=0)
+		{
+			printf("delected\n");
+		}
+		else 
+		{
+			printf("haven't the book\n");
+		}
+	}
+	else if (i==4)
+	{
+		print(*hp);
 	}
 	else
 	{
+		printf("\n");
 		printf("input error\n");
 	}
-	run(head);
+	run(hp);
 	return (0);
 }
 
-struct Book *create(amount){
+struct Book *add(struct Book *head){
 	int i;
-	struct Books *head = NULL, *pnew = NULL;
-	for ( i=0; i<amount; i++)
+	struct Book *pnew = NULL;
+	pnew = (struct Book*)malloc( sizeof (struct Book));
+	printf("the number of the book:\n");
+	scanf("%d",&pnew->num);
+	if (find(head,pnew->num))
 	{
-		pnew = (struct Book*)malloc( sizeof (struct Book));
-		printf("the number of the book:\n");
-		scanf("%d",&pnew->num);
-		printf("the name of the book:\n");
-		scanf("%s",pnew->name);
-		printf("the author of the book:\n");
-		scanf("%s",pnew->author);
-		printf("the publisher of the book:\n");
-		scanf("%s",pnew->publisher);
-		pnew->next = head;
-		head = pnew;
+		printf("already have this number\n");
+	}
+	else
+	{
+	printf("the name of the book:\n");
+	scanf("%s",pnew->name);
+	printf("the author of the book:\n");
+	scanf("%s",pnew->author);
+	printf("the publisher of the book:\n");
+	scanf("%s",pnew->publisher);
+	pnew->next = head;
+	head = pnew;
 	}
 	return (head);
 }
 
 struct Book *find(struct Book *head,int target){
+	int t;
 	while (head)
 	{
-		if ( head->num = target)
+		if ( head->num == target)
 		{
 			t=1;
 			break;
@@ -109,9 +132,46 @@ struct Book *find(struct Book *head,int target){
 }
 
 int delect(struct Book **head,int target){
+	int t=0;
 	while (*head)
 	{
-		if ( (*head)->num = target)
+		if ( (*head)->num == target)
 		{
-			head = head->next;
+			*head = (*head)->next;
+			t = 1;
+			break;
+		}
+		head = &(*head)->next;
+	}
+	return (t);
+}
+
+int print(struct Book *head){
+	printf("\n");
+	printf("num\t");
+	printf("name\t");
+	printf("author\t");
+	printf("publisher\t");
+	printf("\n");
+	while (head)
+	{
+		printf("%d\t",head->num);
+		printf("%s\t",head->name);
+		printf("%s\t",head->author);
+		printf("%s\t",head->publisher);
+		printf("\n");
+		head = head->next;
+	}
+	return (0);
+}
+
+int freememory(struct Book *head){
+	struct Book *p=(void *)head;
+	if (head->next)
+	{
+		head = head->next;
+		freememory(head);
+	}
+	free(head);
+	return (0);
 }
